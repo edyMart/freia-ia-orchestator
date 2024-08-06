2259cdbd818e92406be91e5c25588eae928b8f1c
Edgar <edy@MacBook-Air-de-Edgar.local>
2024-08-06 15:39:19 -0600

    all services from agent

--- routes/Services.py
+++ routes/Services.py
@@ -7,8 +7,36 @@ Agent = agent()
 
 
 @Services_BP.route('description', methods=['POST'])
-def oauth():
+def description():
     body_description = request.json
     response = Agent.description(body_description)
+
+    return response
+
+@Services_BP.route('regionalize', methods=['POST'])
+def regionalize():
+    body_description = request.json
+    response = Agent.description(body_description)
+
+    return response
+
+@Services_BP.route('spellcheck', methods=['POST'])
+def spellcheck():
+    body_description = request.json
+    response = Agent.spellcheck(body_description)
+
+    return response
+
+@Services_BP.route('images/description', methods=['POST'])
+def images_description():
+    body_description = request.json
+    response = Agent.images_description(body_description)
+
+    return response
+
+@Services_BP.route('products/img', methods=['POST'])
+def products_img():
+    body_description = request.json
+    response = Agent.products_img(body_description)
 
     return response
--- services/Agent.py
+++ services/Agent.py
@@ -2,10 +2,9 @@ import requests
 import json
 class Agent:
     def __init__(self):
-        print("")
-    
+        self.url = "https://freia.styrk.io/agent/api/v0"
     def description(self, body):
-        url = "https://freia.styrk.io/agent/api/v0/products/description"
+        url = self.url + "/products/description"
 
         payload = json.dumps(body)
         headers = {
@@ -17,5 +16,50 @@ class Agent:
 
         return response.json()
 
+    def regionalize(self, body):
+        url = self.url + "/regionalize"
 
+        payload = json.dumps(body)
+        headers = {
+            'Content-Type': 'application/json'
+        }
 
+        response = requests.request("POST", url, headers=headers, data=payload)
+
+        return response.json()
+
+    def spellcheck(self, body):
+        url = self.url + "/spellcheck"
+
+        payload = json.dumps(body)
+        headers = {
+            'Content-Type': 'application/json'
+        }
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+
+        return response.json()
+
+    def images_description(self, body):
+        url = self.url + "/images/description"
+
+        payload = json.dumps(body)
+        headers = {
+            'Content-Type': 'application/json'
+        }
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+
+        return response.json()
+
+    def products_img(self, body):
+        url = self.url + "/products/img"
+        
+        payload = json.dumps(body)
+        headers = {
+            'Content-Type': 'application/json'
+        }
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+
+        return response.json()
